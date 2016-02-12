cc1cb1b4318178d64004deb60dc9458e887dfe09
Mithun Patel <mithunster@gmail.com>
2016-02-12 11:25:04 -0600

    Set up initial app

--- test/myexpress.js
+++ test/myexpress.js
@@ -1,13 +1,25 @@
 'use strict';
 let expect = require('chai').expect;
-let assert = require('chai').assert;
 
 let mex = require('../lib/myexpress');
 
 describe('Myexpress', () => {
 
-    it('should return a function upon instantiating', () => {
-        assert.isFunction(mex);
+    it('should be a function', () => {
+        expect(mex).to.be.a('function');
     });
+    
+    
+    it('should return a function upon instantiating', () => {
+        let app = mex();
+        expect(app).to.be.a('function');
+    });
+    
+    
+    it('should have a property called listen that is a function', () => {
+        let app = mex();
+        expect(app).to.have.property('listen');
+    });
+        
 });
     
